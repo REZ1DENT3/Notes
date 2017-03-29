cac95e29967b5ccad1409395eba29e8ccc666d24
REZ1DENT3 <maksim.babichev95@gmail.com>
2017-03-29 16:23:13 +0300

    patch

--- database/migrations/2017_03_29_000001_update_notes_table.php
+++ database/migrations/2017_03_29_000001_update_notes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Notes extends Migration
+class UpdateNotesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,7 +15,7 @@ class Notes extends Migration
     {
         Schema::table('notes', function (Blueprint $table)
         {
-            $table->mediumText('text')->change();
+            $table->string('text', 16777215)->change();
         });
     }
 
